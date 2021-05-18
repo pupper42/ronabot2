8bb6edf594f9228e4df16998916a5a7b131ad4a7
Diluen Cheong <diluen@gmail.com>
2021-05-18 21:26:47 +1000

    Classify this whole thing

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
+// Declare the consts
 require('dotenv').config();
-
-const mfx = require('./mongoFunctions.js');
+const mongoFunctions = require('./mongoFunctions');
 const Discord = require('discord.js');
-const cheerio = require("cheerio");
-
-const dataSource = "https://covidlive.com.au/";
-
+const cheerio = require('cheerio');
+const dataSource = 'https://covidlive.com.au/';
 const client = new Discord.Client();
-
 const defaultSettings = {
     vic: false,
     nsw: false,
@@ -19,14 +16,33 @@ const defaultSettings = {
     constantly_update: false
 }
 
-mfx.crudService("r").catch(console.error);
-mfx.crudService(mode="u", server_id="381685383827554316", updateData={vic: false});
+class RonaBot {
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.guilds.cache.forEach(guild => {
-        console.log(`${guild.name} | ${guild.id}`);
-    });
-});
+    /**
+     * Constructor - load the bot!
+     */
+    constructor() {
+        this.initCrudService();
 
-client.login(process.env.DISCORD_TOKEN);
+        client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}`);
+            client.guilds.cache.forEach(guild => {
+                console.log(`${guild.name} | ${guild.id}`);
+            });
+        });
+
+        client.login(process.env.DISCORD_TOKEN);
+    }
+
+    /**
+     * Initialise the CRUD service
+     */
+    initCrudService() {
+        const mfx = new mongoFunctions();
+        mfx.crudService("r").catch(console.error);
+        mfx.crudService("u", "381685383827554316", {vic: false});
+    }
+}
+
+// Run the bot
+new RonaBot();
