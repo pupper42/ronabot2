23f57a67b84405f50a260b1dfd92cdb1d0568c41
Diluen Cheong <d.cheong@fweb.com.au>
2021-05-20 10:40:59 +1000

    Bot :heart: ardex

--- app/commands/ardex.js
+++ app/commands/ardex.js
@@ -2,6 +2,10 @@ module.exports = {
     name: 'ardex',
     description: 'Ardex! The bot loves you!',
     execute(message) {
-        message.channel.send(':heart:');
+        const embed = {
+            color: '#e31b23',
+            description: ':heart:'
+        };
+        message.channel.send({embed: embed});
     },
 };
