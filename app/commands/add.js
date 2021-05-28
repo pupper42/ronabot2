f5c06ba5692553cb81ab12bdb70af6ad0fcc978c
pupper42 <kev.y2718@gmail.com>
2021-05-28 11:37:37 +1000

    Update add.js
    added let for variables

--- app/commands/add.js
+++ app/commands/add.js
@@ -6,8 +6,8 @@ module.exports = {
     execute(message, args) {
         // TODO: Link to services function to run updater/scraper to grab latest data from db
         // Also use args[0], args[1] to process the user input
-        messageServer = message.guild.id; 
-        newLocation = args.join(" ");
+        let messageServer = message.guild.id; 
+        let newLocation = args.join(" ");
         
         Server.update(messageServer, {$addToSet: {location: [newLocation]}});
 
