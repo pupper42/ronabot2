a52b4c331f24af75b430dfa2b293a3954b968049
Diluen Cheong <diluen@gmail.com>
2021-05-29 14:40:53 +1000

    /ronabot help

---
+++ app/commands/help.js
@@ -0,0 +1,9 @@
+const Server = require('../controllers/server');
+
+module.exports = {
+    name: 'help',
+    description: 'Return list of commands available for use',
+    execute(message) {
+        // Do something
+    },
+};
