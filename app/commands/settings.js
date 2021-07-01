bc09584bb2ef344dc8cc934b9c474d76288ed71c
pupper42 <kev.y2718@gmail.com>
2021-07-01 22:28:48 +1000

    replaced list with settings

--- app/commands/settings.js
+++ app/commands/settings.js
@@ -7,27 +7,35 @@ const Server = require('../controllers/server');
  * @type {{name: string, description: string, execute(*, *): void}}
  */
 module.exports = {
-    name: 'list',
-    description: 'List locations added by a server',
+    name: 'settings',
+    description: 'Show current settings for the server',
     execute(message, args) {
         // Also use args[0], args[1] to process the user input
         let serverId = message.guild.id;
 
-        async function sendLocations() {
+        async function sendSettings() {
+
             let doc = await Server.getDoc(serverId);
+            let updateChannel = message.guild.channels.cache.get(doc.update_channel).name;
+
             const embed = {
                 color: '#ffe360',
                 author: {
                     name: 'RonaBot v2',
                     icon_url: config.discord.icon
                 },
+                title: `Current settings`,
                 fields: [
-                    {name: 'Locations added:', value: doc.location}
+                    {name: 'Locations', value: doc.location},
+                    {name: 'Constantly update?', value: doc.constantly_update},
+                    {name: 'Update interval', value: `${doc.update_interval / 60} minutes`}, 
+                    {name: 'Update channel', value: updateChannel},                    
                 ]
             };
-            await message.channel.send({embed: embed});
+
+            message.channel.send({embed: embed});
         }
 
-        sendLocations();
+        sendSettings();
     },
 };
