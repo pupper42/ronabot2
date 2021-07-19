5b5a539371f634924ef296aeb54c9d66474e8fe2
Diluen Cheong <diluen@gmail.com>
2021-07-19 16:17:07 +1000

    Made status updated_at datetime for readable for the regular human lol

--- app/commands/status.js
+++ app/commands/status.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const Server = require('../controllers/server');
 const MessagingService = require('../services/messagingService');
 
@@ -10,25 +11,17 @@ module.exports = {
     name: 'status',
     description: 'Show current settings and ping for the server',
     execute(message, args) {
-        // Also use args[0], args[1] to process the user input
         let serverId = message.guild.id;
         let updateChannel;
 
         async function getStatus() {
             let server = await Server.getServer(serverId);
-            let updateField;
 
             try {
                 updateChannel = message.guild.channels.cache.get(server.update_channel).name;
             }
             catch {
-                updateChannel = "Not set"
-            }
-
-            if (server.mode === 'repeating') {
-                updateField = {name: 'Last updated', value: server.updated_at};
-            } else if (server.mode === 'scheduled') {
-                updateField = {name: 'Next update', value: server.updated_at};
+                updateChannel = 'Not set';
             }
 
             const fields = {
@@ -38,7 +31,7 @@ module.exports = {
                     {name: 'Constantly update?', value: server.constantly_update},
                     {name: 'Update interval', value: `${server.update_interval} minutes`},
                     {name: 'Update channel', value: updateChannel},
-                    updateField,
+                    {name: (server.mode === 'scheduled') ? "Next update" : "Last updated", value: moment(server.updated_at).format('DD/MM/YYYY HH:mm')},
                     {name: 'Ping', value: `:hourglass: ${Date.now() - message.createdTimestamp}ms, :stopwatch: ${Math.round(message.client.ws.ping)}ms`}
                 ]
             };
