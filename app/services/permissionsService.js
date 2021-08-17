63daa7fcb014c5d8eece5efad875a966c31d7373
Diluen Cheong <diluen@gmail.com>
2021-08-17 12:15:20 +1000

    Fixed permissions check

--- app/services/permissionsService.js
+++ app/services/permissionsService.js
@@ -4,9 +4,10 @@
  * Check if user has permissions
  */
 const MessagingService = require('../services/messagingService');
+const { Permissions } = require('discord.js');
 
 exports.checkPermissions = function(interaction) {
-    if (!(interaction.member.hasPermission("ADMINISTRATOR") || interaction.member.roles.cache.some(r => r.name === "Rona"))) {
+    if (!(interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) || interaction.member.roles.cache.some(r => r.name === "Rona"))) {
         interaction.channel.send({embeds: [MessagingService.getMessage('roleError')]});
         return false;
     }
