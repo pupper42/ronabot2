4494d97bc38bf9255b62b64f272a2d446c0d9ce7
pupper42 <kev.y2718@gmail.com>
2022-02-05 11:09:52 +1100

    fixed date

--- app/services/scraperService.js
+++ app/services/scraperService.js
@@ -86,10 +86,10 @@ exports.getData = async function (url, location) {
         $(firstDoseTable).each((index, element) => {
             let tds = $(element).find("td");
 
-            let total = $(tds[1]).text();
+            let last_updated = $(tds[0]).text();
             let f_dose = $(tds[2]).text();
 
-            let tableRow = {total, f_dose};
+            let tableRow = {last_updated, f_dose};
             firstDoseTableData.push(tableRow);
         });
 
@@ -119,7 +119,8 @@ exports.getData = async function (url, location) {
             deaths: _.isEmpty(deaths) ? '-' : deaths,
             f_dose: _.isEmpty(firstDoseTableData[1]) ? '-' : firstDoseTableData[1].f_dose,
             s_dose: _.isEmpty(secondDoseTableData[1]) ? '-' : secondDoseTableData[1].s_dose,
-            t_dose: _.isEmpty(topBarData[0]) ? '-' : topBarData[0].t_dose
+            t_dose: _.isEmpty(topBarData[0]) ? '-' : topBarData[0].t_dose,
+            last_updated: _.isEmpty(firstDoseTableData[1]) ? '-' : firstDoseTableData[1].lastUpdated
         }
 
     // Save to database
